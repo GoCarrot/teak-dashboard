ea8fd24a124b15ee291e507898475734338be1f8
Pat Wilson <pat@teak.io>
2019-08-20 12:10:57 -0700

    Remove breathe

--- conf.py
+++ conf.py
@@ -31,14 +31,7 @@ import subprocess
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ "breathe" ]
-
-breathe_projects = {
-    "teak":"_doxygen/xml/",
-}
-
-breathe_default_project = "teak"
-
+extensions = []
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
